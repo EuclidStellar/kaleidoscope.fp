5c60631c3492ec9dac9def1d55cf46dc60a86332
Euclid Stellar <euclidstellar@gmail.com>
2023-12-13 19:26:22 +0530

    events done

--- lib/model/eventsmodel.dart
+++ lib/model/eventsmodel.dart
@@ -3,28 +3,31 @@ class Event {
   final DateTime date;
   final String description;
   final bool isUpcoming;
+  final String imageURL; // Add this line
 
   Event({
     required this.name,
     required this.date,
     required this.description,
     required this.isUpcoming,
+    required this.imageURL, // Add this line
   });
 }
 
-
 List<Event> events = [
   Event(
-    name: 'Cultural Night 2022',
+    name: 'Anti Ragging Poster Making Competetion 2023',
     date: DateTime(2022, 10, 15),
     description: 'A night of music, dance, and cultural celebration.',
     isUpcoming: false,
+    imageURL: 'https://firebasestorage.googleapis.com/v0/b/footprints-app-c3790.appspot.com/o/images%2FScreenshot%202023-12-13%20at%207.12.33%E2%80%AFPM.png?alt=media&token=28eb5288-fc62-486f-84fd-be3ea7e3e0dd',
   ),
   Event(
-    name: 'Spring Festival 2023',
+    name: 'Chayan 2024',
     date: DateTime(2023, 3, 20),
-    description: 'Celebrate the arrival of spring with various activities.',
+    description: 'Recruitment Drive of Team Footprints',
     isUpcoming: true,
+    imageURL: 'https://firebasestorage.googleapis.com/v0/b/footprints-app-c3790.appspot.com/o/images%2FScreenshot%202023-12-13%20at%207.12.33%E2%80%AFPM.png?alt=media&token=28eb5288-fc62-486f-84fd-be3ea7e3e0dd',
   ),
   // Add more events as needed
 ];
--- lib/screens/events.dart
+++ lib/screens/events.dart
@@ -1,3 +1,49 @@
+// import 'package:flutter/material.dart';
+// import 'package:kaleidoscope_fp/screens/registration.dart';
+
+// import '../model/eventsmodel.dart';
+
+// class EventsTab extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Events'),
+//       ),
+//       body: ListView.builder(
+//         itemCount: events.length,
+//         itemBuilder: (context, index) {
+//           final event = events[index];
+//           return ListTile(
+//             leading: Image.network(
+//               event.imageURL,
+//               width: 50, // Adjust the width as needed
+//               height: 50, // Adjust the height as needed
+//               fit: BoxFit.cover,
+//             ),
+//             title: Text(event.name),
+//             subtitle: Text(event.description),
+//             trailing: event.isUpcoming
+//                 ? ElevatedButton(
+//                     onPressed: () {
+//                       Navigator.push(
+//                         context,
+//                         MaterialPageRoute(
+//                           builder: (context) => RegistrationScreen(),
+//                         ),
+//                       );
+//                     },
+//                     child: Text('Register'),
+//                   )
+//                 : null,
+//           );
+//         },
+//       ),
+//     );
+//   }
+// }
+
+
 import 'package:flutter/material.dart';
 import 'package:kaleidoscope_fp/screens/registration.dart';
 
@@ -14,26 +60,73 @@ class EventsTab extends StatelessWidget {
         itemCount: events.length,
         itemBuilder: (context, index) {
           final event = events[index];
-          return ListTile(
-            title: Text(event.name),
-            subtitle: Text(event.description),
-            trailing: event.isUpcoming
-                ? ElevatedButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) =>
-                              RegistrationScreen(),
-                        ),
-                      );
-                    },
-                    child: Text('Register'),
-                  )
-                : null,
+          return GestureDetector(
+            onTap: () {
+              _showEventDetailsDialog(context, event);
+            },
+            child: ListTile(
+              leading: Image.network(
+                event.imageURL,
+                width: 50, // Adjust the width as needed
+                height: 50, // Adjust the height as needed
+                fit: BoxFit.cover,
+              ),
+              title: Text(event.name),
+              subtitle: Text(event.description),
+              trailing: event.isUpcoming
+                  ? ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => RegistrationScreen(),
+                          ),
+                        );
+                      },
+                      child: Text('Register'),
+                    )
+                  : null,
+            ),
           );
         },
       ),
+    );
+  }
+
+  void _showEventDetailsDialog(BuildContext context, Event event) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          content: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Image.network(
+                event.imageURL,
+                fit: BoxFit.cover,
+              ),
+              SizedBox(height: 8.0),
+              Text(
+                event.name,
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18.0,
+                ),
+              ),
+              SizedBox(height: 8.0),
+              Text(event.description),
+              SizedBox(height: 16.0),
+              ElevatedButton(
+                onPressed: () {
+                  // Implement the functionality for the "View Gallery" button
+                },
+                child: Text('View Gallery'),
+              ),
+            ],
+          ),
+        );
+      },
     );
   }
 }
