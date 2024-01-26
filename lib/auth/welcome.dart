56316c3de032d78ebbbd769717eee5924a850629
Euclid Stellar <euclidstellar@gmail.com>
2024-01-27 00:20:35 +0530

    v1

--- lib/auth/welcome.dart
+++ lib/auth/welcome.dart
@@ -11,99 +11,101 @@ class Welcome extends StatelessWidget {
         body: SafeArea(
             child: Padding(
       padding: const EdgeInsets.all(16.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const Text(
-            'Welcome to \nFootPrints',
-            textAlign: TextAlign.start,
-            style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-          ),
-          const SizedBox(
-            height: 30,
-          ),
-          const Align(
-            alignment: Alignment.center,
-            child: Text(
-              'create    Inspire    Innovate',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.w700,
-                  color: Color.fromARGB(255, 46, 139, 193)),
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              'Welcome to \nFootPrints',
+              textAlign: TextAlign.start,
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
             ),
-          ),
-          const SizedBox(
-            height: 90,
-          ),
-          Center(
-            child: SizedBox(
-              width: double.infinity,
-              
-              child: Image.asset('assets/images/img1.png'),
+            const SizedBox(
+              height: 30,
             ),
-          ),
-          const SizedBox(
-            height: 40, // Add some spacing between the images
-          ),
-          // Center(
-          //   child: SizedBox(
-          //     // width: double.infinity,
-          //     width: MediaQuery.of(context).size.width * 0.25,
-          //     //height: MediaQuery.of(context).size.height * 0.75,
-          //     child: Image.asset(
-          //       'assets/images/brl_logo.png',
-          //       fit: BoxFit.scaleDown,
-          //     ),
-          //   ),
-          // ),
-          const SizedBox(
-            height: 60,
-          ),
-          Container(
-            height: 140,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-              color: Colors.white,
+            const Align(
+              alignment: Alignment.center,
+              child: Text(
+                'create    Inspire    Innovate',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w700,
+                    color: Color.fromARGB(255, 46, 139, 193)),
+              ),
             ),
-            child: Column(
-              children: [
-               
-               
-                MyTextButton(
-                  buttonName: 'Sign Up',
-                  onTap: () {
-                    Navigator.push(
+            const SizedBox(
+              height: 90,
+            ),
+            Center(
+              child: SizedBox(
+                width: double.infinity,
+                
+                child: Image.asset('assets/images/img1.png'),
+              ),
+            ),
+            const SizedBox(
+              height: 40, // Add some spacing between the images
+            ),
+            // Center(
+            //   child: SizedBox(
+            //     // width: double.infinity,
+            //     width: MediaQuery.of(context).size.width * 0.25,
+            //     //height: MediaQuery.of(context).size.height * 0.75,
+            //     child: Image.asset(
+            //       'assets/images/brl_logo.png',
+            //       fit: BoxFit.scaleDown,
+            //     ),
+            //   ),
+            // ),
+            const SizedBox(
+              height: 60,
+            ),
+            Container(
+              height: 140,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: Colors.white,
+              ),
+              child: Column(
+                children: [
+                 
+                 
+                  MyTextButton(
+                    buttonName: 'Sign Up',
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) =>  SignUp(),
+                          ));
+                    },
+                    bgColor: Colors.black,
+                    textColor: Colors.white,
+                  ),
+                     const SizedBox(
+                    height : 10,
+                  ),
+                   MyTextButton(
+                    buttonName: 'Login',
+                    onTap: () {
+                      Navigator.push(
                         context,
                         MaterialPageRoute(
-                          builder: (context) =>  SignUp(),
-                        ));
-                  },
-                  bgColor: Colors.black,
-                  textColor: Colors.white,
-                ),
-                   const SizedBox(
-                  height : 10,
-                ),
-                 MyTextButton(
-                  buttonName: 'Login',
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => const LoginScreen(
-                          title: 'Login',
+                          builder: (context) => const LoginScreen(
+                            title: 'Login',
+                          ),
                         ),
-                      ),
-                    );
-                  },
-                  bgColor: Colors.black,
-                  textColor: Colors.white,
-                ),
-              ],
-            ),
-          )
-        ],
+                      );
+                    },
+                    bgColor: Colors.black,
+                    textColor: Colors.white,
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     )));
   }
